ac5a87a6bdd0e3706c0e82fb9693526976eae2dd
felixkhaw <wzr955@haw-hamburg.de>
2026-05-11 13:50:00 +0200

    Rechtschreib Korrektur

--- Programs/PrimzahlSiebASM/Src/main.s
+++ Programs/PrimzahlSiebASM/Src/main.s
@@ -8,9 +8,9 @@
 
 ; Zweite Idee näher am Sieb des Eratosthenes
 ; Ein loop der zahlen array durchläuft und die vielfachen von primzahlen löscht
-; Bsp in zahlen stehen die Zahlen 11-1000
-; in Primzahlen stehen die Primzahlen von 1-10 also 2,3,5,7
-; jetzt durcläuft der loop die Zahle 11-1000 und löscht alle vielfachen von 7
-; danach von das gleiche mit dem vielfachen von 5 u.s.w
-; Am Ende stehen alle Primazahlen bois 1000 im array Zahlen
-; Vorteil es werden zunehmend weniger Zahlen durch das löschen und dadurch gibt es insgesamt weniger durchläufe.
+; Bsp in Zahlen(Array) stehen die Zahlen 11-1000
+; in Primzahlen(Array) stehen die Primzahlen von 1-10 also 2,3,5,7
+; jetzt durchläuft der Loop das Zahlen(Array) 11-1000 und löscht alle Vielfachen von 7
+; danach nochmal mit dem Vielfachen von 5 u.s.w
+; Am Ende stehen alle Primzahlen bis 1000 im Zahlen(Array)
+; Vorteil: es werden zunehmend weniger Zahlen, durch das löschen und es gibt insgesamt weniger Durchläufe
