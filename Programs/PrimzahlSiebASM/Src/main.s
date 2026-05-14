4b57c78b987b57cf8c5be45d07497683f0c57ed1
felixkhaw <wzr955@haw-hamburg.de>
2026-05-14 20:55:47 +0200

    Hinzufügen von Pseudocode in eigenen Worten

--- Programs/PrimzahlSiebASM/Src/main.s
+++ Programs/PrimzahlSiebASM/Src/main.s
@@ -1,8 +1,15 @@
 ; Pseudocode: https://github.com/felixkhaw/Primzahlen
 
-; Vorbereitung Assembler code
-; primzahlen DCB 2,3,5,7,11 ; Definieren von Array mit Primzahlen von 2-11
-; zahlen FILL 999,0 ; Definieren von Array mit Zahlen von 2-1000 / Wenn man das so macht muss noch überprüft werden welche Größe FILL anlegt
+; Wesentliche Schritte:
+; Anlegen von Array mit Primzahlen von 2-10
+; Anlegen von Array mit Zahlen von 2 -1000
 
-; for_01
-;   mov r0, #1
+; Eine for-Schleife mit Variable i, die Primzahlen durchläuft und bei jeden Durchlauf Folgendes macht:
+; Eine for-Schleife mit Variable j, die Zahlen durchläuft und Folgendes macht:
+; Schauen ob die Zahl auf der die Schleife gerade steht ohne Rest durch die Primzahl teilbar ist auf der die Äußere For-Schleife steht.
+; Wenn ja dann soll die Zahl eliminiert werden
+; Wenn nein nichts
+; in beiden Fällen j++
+; Am Ende des inneren Durchlaufes i++
+
+; Ausgabe des Zahlenarrays in dem jetzt alle Primzahlen stehen
