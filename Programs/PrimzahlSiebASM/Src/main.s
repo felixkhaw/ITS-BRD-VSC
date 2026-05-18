93afe8c393af4ed44d35d4bbb95eeeaaf3f3b193
felixkhaw <wzr955@haw-hamburg.de>
2026-05-18 19:36:40 +0200

    Hinzufügen von Pseudocode in eigenen Worten

--- Programs/PrimzahlSiebASM/Src/main.s
+++ Programs/PrimzahlSiebASM/Src/main.s
@@ -1,5 +1,7 @@
 ; Pseudocode: https://github.com/felixkhaw/Primzahlen
 
 ; Wesentliche Schritte:
-; Anlegen von Boolean Array mit Zahlen von 2 -1000
-;  
+; Anlegen von Boolean Array mit 1000 Feldern; Index repräsentiert die Zahl
+; Schrittweises Druchlaufen des Boolean Array -> Schleifenvariable symbolisiert die Zahl
+; Für jeden Schritt Vielfaches der Indexzahl aus Boolean Array streichen -> Streichen bedeutet Wert an Stelle x auf False setzen
+; Am Ende Ausgabe im Sinne einer For-Schleife des Variable als Index benutzt wird und mit einer If Afrage nur Werte ausgibt des Wert auf True sind
