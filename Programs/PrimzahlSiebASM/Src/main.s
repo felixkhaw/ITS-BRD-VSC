5f75ff75e7ff847f565d3b5d96b97bbb70934f37
felixkhaw <wzr955@haw-hamburg.de>
2026-05-24 09:37:51 +0200

    Starten des Assembler codes für PA 5

--- Programs/PrimzahlSiebASM/Src/main.s
+++ Programs/PrimzahlSiebASM/Src/main.s
@@ -1,7 +1,43 @@
 ; Pseudocode: https://github.com/felixkhaw/Primzahlen
 
-; Wesentliche Schritte:
-; Anlegen von Boolean Array mit 1000 Feldern; Index repräsentiert die Zahl
-; Schrittweises Druchlaufen des Boolean Array -> Schleifenvariable symbolisiert die Zahl
-; Für jeden Schritt Vielfaches der Indexzahl aus Boolean Array streichen -> Streichen bedeutet Wert an Stelle x auf False setzen
-; Am Ende Ausgabe im Sinne einer For-Schleife des Variable als Index benutzt wird und mit einer If Afrage nur Werte ausgibt des Wert auf True sind
+;************************************************
+;* Beginn der globalen Daten *
+;************************************************
+                   AREA MyData, DATA, align = 2
+Base
+
+; Reservieren eines 4 Byte Arrays mit Werten
+zahlen              FILL    1000, 0x01
+                    DCB     0x00 // Terminierungsvariable
+
+; dem Programm verfügbar machen
+                    EXPORT zahlen
+
+
+;***********************************************
+;* Beginn des Programms *
+;************************************************
+    AREA |.text|, CODE, READONLY, ALIGN = 3
+; ----- S t a r t des Hauptprogramms -----
+                EXPORT main
+                EXTERN initITSboard
+
+; Start der main Schleife des Programmes
+; r0 -> Zeiger, r1 -> Counter, r2 -> Wert
+main            PROC
+                bl    initITSboard                 ; HW Initialisieren
+                ldrb  r0,=zahlen    ; Start Adresse von Zahlen
+                ldrb  r2, [r0]
+                mov   r1, 0x00      ; Initialisieren des Counters
+
+for-zahlen      mov   r0, 0x00      
+until-zahlen    cpm   r2, 0x00   
+do-zahlen       
+                // If Struktur aufrufen 
+step-zahlen     add r1,r1, #1
+enddo-zahlen
+
+
+forever         b   forever
+                ENDP
+                END
