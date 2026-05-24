c375b6cf11c2a1a69d2c702fb538623087377bfb
felixkhaw <wzr955@haw-hamburg.de>
2026-05-24 09:59:53 +0200

    Erzeuge for Struktur vielfaches

--- Programs/PrimzahlSiebASM/Src/main.s
+++ Programs/PrimzahlSiebASM/Src/main.s
@@ -23,10 +23,17 @@ zahlen              FILL    1000, 0x01
                 EXTERN initITSboard
 
 ; Start der main Schleife des Programmes
-; r0 -> Zeiger, r1 -> Counter, r2 -> Wert, r3 -> Primzahl
+;   r0 -> Zeiger zahlen
+;   r1 -> Counter zahlen
+;   r2 -> Wert zahlen
+;   r3 -> Primzahl
+;   r4 -> Zeiger vielfaches
+;   r5 -> Counter vielfaches
+;   r6 -> Wert vielfaches
 main            PROC
                 bl    initITSboard                 ; HW Initialisieren
                 ldrb  r0,=zahlen    ; Start Adresse von Zahlen
+                ldrb  r4,=zahlen    ; Start Adresse von Zahlen
                 ldrb  r2, [r0]
                 mov   r1, 0x00      ; Initialisieren des Counters
 
@@ -36,6 +43,13 @@ do-zahlen
                 // If Struktur aufrufen 
 step-zahlen     add r1,r1, #1
 enddo-zahlen
+
+for-vielfaches      mov   r5, 0x00      
+until-vielfaches    cpm   r4, 0x00  
+do-vielfaches       
+                // Setzte bei Adresse x in Zahlen Wert auf 0x00
+step-vielfaches     add r4,r4, #1
+enddo-vielfaches
 
 
 forever         b   forever
