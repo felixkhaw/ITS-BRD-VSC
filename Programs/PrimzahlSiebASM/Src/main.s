b2e91edafcae26b9f3665798350714679d4a2278
felixkhaw <felix.knupper@haw-hamburg.de>
2026-05-26 23:33:13 +0200

    Neu planen der Registerbelegung und Anpassung der Schleifen bzw. Logik

--- Programs/PrimzahlSiebASM/Src/main.s
+++ Programs/PrimzahlSiebASM/Src/main.s
@@ -8,7 +8,7 @@ Base
 
 ; Reservieren eines 4 Byte Arrays mit Werten
 zahlen              FILL    1000, 0x01
-                    DCB     0xFF // Terminierungsvariable
+                    DCB     0xFF    ; Terminierungsvariable
 
 primzahlen          FILL    500, 0
                     DCB     0xFF
@@ -25,27 +25,22 @@ primzahlen          FILL    500, 0
                 EXPORT main
                 EXTERN initITSboard
 
-; Start der main Schleife des Programmes
-;   r0 -> Zeiger zahlen
-;   r1 -> Wert zahlen
-;   r2 -> Primzahl
-;   r3 -> Zeiger Vielfaches
-;   r4 -> Wert Vielfaches
-;   r5 -> Sprung Vielfaches
-;   r6 -> Start primzahl
+;   r0 -> Zeiger auf aktuelle Zahl in zahlen
+;   r1 -> Wert bzw. Boolean an [r0]
+;   r2 -> Startadresse zahlen
+;   r3 -> aktuelle Zahl / Schrittweise
+;   r4 -> aktuelles Vielfaches als Zahl
+;   r5 -> Zeiger auf aktuelles Vielfaches
+;   r6 -> Zeiger primzahl
+
 main            PROC
                 bl    initITSboard ; HW Initialisieren
                 ldr  r0,=zahlen    ; Start Adresse von Zahlen
-                add  r0,r0, #2
+                add  r0,r0, #1     ; Start von 2 also zweiter Speicherblock
                 ldr  r6,=primzahl
-
-; ******************
-; FOR Zahlen
-; Info -> Counter ist nicht nötig wegen Terminierungsvariable
-; ******************
-for_zahlen      // Sprungziel 
+for_zahlen
 until_zahlen    
-                ldrb  r1, [r0]      ; Lade Wert aus Adress r0
+                ldrb  r1, [r0]      ; Lade Wert aus Adresse r0
                 cmp   r1, #0xFF     ; Prüfe ob Terminator erreicht ist
                 beq   enddo_zahlen  ; Springe wenn Terminator erreicht ist -> r2=0
 do_zahlen   
@@ -53,65 +48,37 @@ if_gestrichen
                 cmp r1, #1
                 bne endif_gestrichen
 then_gestrichen 
+                ldr r2, =zahlen
                 b for_vielfaches
+for_vielfaches
+                sub r3, r0, r2 ; Index
+                add r3, r3, #1 ; Für Zahl -> Index + 1
+until_vielfaches
+                cmp r3, #1000
+                bgt enddo_vielfaches
+do_vielfaches   
+                
+step_vielfaches     
+                add r3, r3, r4
+                b until_vielfaches
+enddo_vielfaches
 endif_gestrichen
 step_zahlen     
                 add r0,r0, #1
                 b until_zahlen
 enddo_zahlen    
-                b step_zahlen
+                ldr  r0,=zahlen
 
-; ******************
-; FOR Vielfaches
-; ******************
-;   r0 -> Zeiger zahlen
-;   r1 -> Wert zahlen
-;   r2 -> Primzahl
-;   r3 -> Zeiger Vielfaches
-;   r4 -> Wert Vielfaches
-;   r5 -> Sprung Vielfaches
-;   r6 -> Start Primzahl
-
-for_vielfaches      // Sprungziel
-until_vielfaches
-                cmp 
-                blt enddo_vielfaches
-do_vielfaches   
-                r3, =zahlen
-                sub r4, r0, r3 \\ Im ersten Durchlauf müsste 2 darin stehen
-                strb r4, [r3]
-step_vielfaches     
-                add r3, r3, r4
-                b until_vielfaches
-enddo_vielfaches
-                b step_vielfaches
+; Ausgabe der Primzahlen in primzahl Array
+; for_primzahl
+; until_primzahl
+; do_primzahl  
+; if_primzahl         
+; then_primzahl 
+; endif_primzahl        
+; step_primzahl     
+; enddo_primzahl
 
 forever         b   forever
                 ENDP
                 END
-
-; Sequentieller Ablauf nach Beendigung der ersten beiden Scheifen in zahlen stehen gestrichen die Primzahlen
-                ldr  r0,=zahlen
-
-; Ausgabe der Primzahlen in primzahl Array
-; for_primzahl     // Sprungziel
-; until_primzahl
-;                cmp   r0, #0xFF
-;                beq   enddo_primzahl
-; do_primzahl  
-; if_primzahl
-;                ldrb r1,[r0]
-;                cmp r1, #1
-;                bne endif_gestrichen
-; then_primzahl 
-;                b for_vielfaches
-; endif_primzahl        
-; step_primzahl     
-;                add r0,r0, #1 
-;                b until_primzahl
-; enddo_primzahl
-;                b step_primzahl
-
-; forever         b   forever
-;                ENDP
-;                END
