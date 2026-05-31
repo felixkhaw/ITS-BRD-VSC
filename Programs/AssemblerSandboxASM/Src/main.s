4f53640bef4dfd80da55281b704e55463bd08575
felixkhaw <felix.knupper@haw-hamburg.de>
2026-05-31 21:45:41 +0200

    LED Test und Frage zu GPIO_F_PIN

--- Programs/AssemblerSandboxASM/Src/main.s
+++ Programs/AssemblerSandboxASM/Src/main.s
@@ -1,24 +1,20 @@
-;************************************************
-;* Beginn der globalen Daten *
-;************************************************
-                   AREA MyData, DATA, align = 2
-Base
+    AREA MyData, DATA, align = 3
 
-data	DCW		10, 0xFF, 2		
-bmask	EQU		0x2	; 0b0010
+GPIO_F_PIN      EQU     0x40000000 ; noch nicht der richtige Adressbereich
+data            DCW     10, 0xFF, 2		
+bmask           EQU		0x2             ;0b0010
                     
-;***********************************************
-;* Beginn des Programms *
-;************************************************
+
     AREA |.text|, CODE, READONLY, ALIGN = 3
-; ----- S t a r t des Hauptprogramms -----
+
                 EXPORT main
                 EXTERN initITSboard
 
 main            PROC
                 bl    initITSboard ; HW Initialisieren
-
-				MOV	    r0, #bmask		
+		        LDR     r0, =GPIO_F_PIN
+                MOV     r1, #bmask
+                LDR     r1, [r0]  
 
 forever         b   forever
                 ENDP
