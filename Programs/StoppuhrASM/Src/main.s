57451a49975cc2d9955528862d44e5c9ca38af47
felixkhaw <felix.knupper@haw-hamburg.de>
2026-06-05 00:23:21 +0200

    Starte Stoppuhr Projekt und teste Unterprogrammaufruf

---
+++ Programs/StoppuhrASM/Src/main.s
@@ -0,0 +1,99 @@
+; Define address of selected GPIO and Timer registers
+PERIPH_BASE     	equ	0x40000000                 ;Peripheral base address
+AHB1PERIPH_BASE 	equ	(PERIPH_BASE + 0x00020000)
+APB1PERIPH_BASE     equ PERIPH_BASE
+
+GPIOD_BASE			equ	(AHB1PERIPH_BASE + 0x0C00)
+GPIOF_BASE			equ	(AHB1PERIPH_BASE + 0x1400)
+TIM2_BASE           equ (APB1PERIPH_BASE + 0x0000)
+	
+GPIO_F_PIN        	equ	(GPIOF_BASE + 0x10)
+
+GPIO_D_PIN			equ	(GPIOD_BASE + 0x10)
+GPIO_D_SET			equ (GPIOD_BASE + 0x18)
+GPIO_D_CLR			equ	(GPIOD_BASE + 0x1A)
+	
+TIMER				equ (TIM2_BASE + 0x24)   ; CNT : current time stamp (32 bit),  resolution
+TIM2_PSC			equ (TIM2_BASE + 0x28)   ; Prescaler  resolution
+TIM2_ERG			equ (TIM2_BASE + 0x14)   ; 16 Bit register, Bit 0 : 1 Restart Timer
+
+
+    EXTERN initITSboard
+    EXTERN GUI_init
+	EXTERN TP_Init
+	EXTERN initTimer
+	EXTERN lcdSetFont
+	EXTERN lcdGotoXY      		; TFT goto x y function
+	EXTERN lcdPrintS			; TFT output function	
+    EXTERN lcdPrintC            ; TFT output one character		
+	EXTERN Delay				; Delay (ms) function
+
+
+;********************************************
+; Data section, aligned on 4-byte boundery
+;********************************************
+	AREA MyData, DATA, align = 2
+
+DEFAULT_BRIGHTNESS	DCW     800
+MY_TEXT				DCB		"Hold down different buttons from S0 to S7 and watch D8 to D15.", 0
+
+;********************************************
+; Code section, aligned on 8-byte boundery
+;********************************************
+	AREA |.text|, CODE, READONLY, ALIGN = 3
+
+
+;--------------------------------------------
+; main subroutine
+;--------------------------------------------
+	EXPORT main [CODE]
+	
+main	PROC
+
+		; Initialisierung der HW
+		BL		initITSboard
+		ldr   	r1, =DEFAULT_BRIGHTNESS
+		ldrh 	r0, [r1]
+		bl   	GUI_init
+		bl  	initTimer
+		ldr 	R1,=TIM2_PSC   			; Set pre scaler such that 1 timer tick represents 10 us
+		mov 	R0,#(90*10-1) 
+		strh	R0,[R1]
+		ldr 	R1,=TIM2_ERG   			; Restart timer	
+		mov		R0,#0x01
+		strh	R0,[R1]					; Set UG Bit
+		MOV 	R0, #24
+		bl  	lcdSetFont
+		BL	    test
+		; Ihre Initialisierung
+
+		; Simple test code
+		LDR 	R0,=MY_TEXT
+		BL  	lcdPrintS
+superloop
+		; read buttons
+		LDR		R0,=GPIO_F_PIN
+		ldrh	R0,[R0]
+		and		R0,#0xFF   ; set bit 31 to 8 of R0 to 0 ; bit 7 to 0 do not change
+		; bit i for R0 is 1 <=> button S<i> not pressed (for 0 <= i <= 7)
+		; bit i for R0 is 0 <=> button S<i>     pressed (for 0 <= i <= 7)
+		
+		; switch LEDs off (button s<i> not pressed : LED D<�+8> switched off (for 0 <= i <= 7)
+		LDR		R1,=GPIO_D_CLR
+		str		R0,[R1]
+		
+		; switch LEDs on (button s<i>      pressed : LED D<�+8> switched on  (for 0 <= i <= 7)
+		eor		R1,R1,#0xFF       ; toogle bit 0 to 7 of R1
+		LDR		R1,=GPIO_D_SET
+		str		R0,[R1]	
+		BAL		superloop				; End of superloop
+		ENDP
+
+test PROC
+		MOV	    R0, #3
+		ADD	    R0, #3
+		BX lr
+ENDP
+
+		ALIGN
+		END
